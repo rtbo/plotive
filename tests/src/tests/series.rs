65d81a5813eff65834c6425dd1b051a4e7236934
Rémi Thebault <remi.thebault@gmail.com>
2026-05-08 18:32:42 +0200

    interp tests now in series

--- tests/src/tests/series.rs
+++ tests/src/tests/series.rs
@@ -3,6 +3,12 @@ use plotive::{data, des, style};
 use crate::tests::fig_small;
 use crate::{TestHarness, assert_fig_eq_ref};
 
+fn line() -> des::series::Line {
+    let x = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+    let y = vec![0.0, 2.0, 3.0, 1.0, 4.0, 4.0];
+    des::series::Line::new(des::data_inline(x), des::data_inline(y)).into()
+}
+
 #[test]
 fn series_line_nodata() {
     let plot = des::Plot::new(vec![
@@ -15,6 +21,56 @@ fn series_line_nodata() {
     let fig = fig_small(plot);
 
     assert_fig_eq_ref!(&fig, "series/line-nodata");
+}
+
+#[test]
+fn series_line_interp_linear() {
+    let plot = line()
+        .with_interpolation(des::series::Interpolation::Linear)
+        .into_plot();
+    let fig = fig_small(plot);
+
+    assert_fig_eq_ref!(&fig, "series/line-interp-linear");
+}
+
+#[test]
+fn series_line_interp_step_early() {
+    let plot = line()
+        .with_interpolation(des::series::Interpolation::StepEarly)
+        .into_plot();
+    let fig = fig_small(plot);
+
+    assert_fig_eq_ref!(&fig, "series/line-interp-step-early");
+}
+
+#[test]
+fn series_line_interp_step_middle() {
+    let plot = line()
+        .with_interpolation(des::series::Interpolation::StepMiddle)
+        .into_plot();
+    let fig = fig_small(plot);
+
+    assert_fig_eq_ref!(&fig, "series/line-interp-step-middle");
+}
+
+#[test]
+fn series_line_interp_step_late() {
+    let plot = line()
+        .with_interpolation(des::series::Interpolation::StepLate)
+        .into_plot();
+    let fig = fig_small(plot);
+
+    assert_fig_eq_ref!(&fig, "series/line-interp-step-late");
+}
+
+#[test]
+fn series_line_interp_spline() {
+    let plot = line()
+        .with_interpolation(des::series::Interpolation::Spline)
+        .into_plot();
+    let fig = fig_small(plot);
+
+    assert_fig_eq_ref!(&fig, "series/line-interp-spline");
 }
 
 #[test]
