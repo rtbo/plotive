7d6312bd54b1f496fa48bbb5f2db341701c89b04
Rémi Thebault <remi.thebault@gmail.com>
2026-05-31 13:41:46 +0200

    default run_all_examples with png

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "No output format specified, defaulting to png"
+    set -- png
+fi
 
 cargo run --example text_line --package plotive-text --features noto-sans
 cargo run --example text_rich --package plotive-text --features noto-sans,noto-serif
 
-cargo run --example area -- $@
-cargo run --example bars -- $@
-cargo run --example gauss -- $@
-cargo run --example sine -- $@
+# passing all needed feature to each example to avoid unnecessary recompilation
+features="data-csv,dsl,noto-sans,noto-serif-italic,time,utils"
 
-cargo run --example bouncing_ball --features time -- $@
-
-cargo run --example bode_rlc --features noto-serif-italic,utils -- $@
-
-cargo run --example multiple_axes --features utils -- $@
-cargo run --example subplots --features utils -- $@
-
-cargo run --example bitcoin --features data-csv,time -- $@
-cargo run --example iris --features data-csv -- $@
-cargo run --example stars --features data-csv -- $@
-
-cargo run --example bode_rlc_dsl --features dsl,noto-serif-italic,utils -- $@
-
-cargo run --example iris_dsl --features data-csv,dsl -- $@
-
-cargo run --example multiple_axes_dsl --features dsl,utils -- $@
-cargo run --example subplots_dsl --features dsl,utils -- $@
+cargo run --example area --features $features -- $@
+cargo run --example bars --features $features -- $@
+cargo run --example bitcoin --features $features -- $@
+cargo run --example bode_rlc --features $features -- $@
+cargo run --example bode_rlc_dsl --features $features -- $@
+cargo run --example bouncing_ball --features $features -- $@
+cargo run --example gauss --features $features -- $@
+cargo run --example iris --features $features -- $@
+cargo run --example iris_dsl --features $features -- $@
+cargo run --example multiple_axes --features $features -- $@
+cargo run --example multiple_axes_dsl --features $features -- $@
+cargo run --example sine --features $features -- $@
+cargo run --example stars --features $features -- $@
+cargo run --example subplots --features $features -- $@
+cargo run --example subplots_dsl --features $features -- $@
